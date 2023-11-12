1c489954a0e84643cb8e7131ca911aaf94649e1b
venice <44522756+venicelord@users.noreply.github.com>
2023-11-12 23:43:20 +0800

    Update auto_shutdown.sh

--- auto_shutdown.sh
+++ auto_shutdown.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
-TRAFF_TOTAL=980 #改成自己的预定额度，建议稍小些，单位GB。
-TRAFF_USED=$(vnstat --oneline b | awk -F';' '{print $11}')
-CHANGE_TO_GB=$(expr $TRAFF_USED / 1073741824)
 
-if [ $CHANGE_TO_GB -gt $TRAFF_TOTAL ]; then
+TRAFF_TOTAL=800#改成自己的预定额度，建议稍小些，单位GB;
+TRAFF_DAY_TOTAL=1 #改成自己的预定额度，建议稍小些，单位GB;
+
+
+TRAFF_USED=$(vnstat --oneline b | awk -F';' '{print $11}')
+MONTH_GB=$(expr $TRAFF_USED / 1073741824)
+
+TRAFF_DAY_USED=$(vnstat --oneline b | awk -F';' '{print $6}')
+DAY_GB=$(expr $TRAFF_DAY_USED / 1073741824)
+
+if [ $MONTH_GB -gt $TRAFF_TOTAL ]; then
     shutdown -h now
 fi
+
+if [ $DAY_GB -gt $TRAFF_DAY_TOTAL ]; then
+    shutdown -h now
+fi
+
