747ef359072eb14db21810560f370eb1a2485d01
venice <44522756+venicelord@users.noreply.github.com>
2023-11-12 21:52:23 +0800

    Create auto_shutdown.sh

---
+++ auto_shutdown.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+TRAFF_TOTAL=980 #改成自己的预定额度，建议稍小些，单位GB。
+TRAFF_USED=$(vnstat --oneline b | awk -F';' '{print $11}')
+CHANGE_TO_GB=$(expr $TRAFF_USED / 1073741824)
+
+if [ $CHANGE_TO_GB -gt $TRAFF_TOTAL ]; then
+    shutdown -h now
+fi
