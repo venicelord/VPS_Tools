ea32558702317de336642e3ea768e048bd9ec4ff
venice <44522756+venicelord@users.noreply.github.com>
2023-11-13 22:44:57 +0800

    Update auto_shutdown.sh

--- auto_shutdown.sh
+++ auto_shutdown.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 TRAFF_TOTAL=800 #改成自己的预定额度，建议稍小些，单位GB;
-TRAFF_DAY_TOTAL=2 #改成自己的预定额度，建议稍小些，单位GB;
+TRAFF_DAY_TOTAL=3 #改成自己的预定额度，建议稍小些，单位GB;
 
 
 TRAFF_USED=$(vnstat --oneline b | awk -F';' '{print $11}')
