771718c27403231e95377e9386d74f69f9399876
venice <44522756+venicelord@users.noreply.github.com>
2023-11-20 00:44:11 +0800

    Update auto_shutdown.sh

--- auto_shutdown.sh
+++ auto_shutdown.sh
@@ -3,8 +3,6 @@
 TRAFF_TOTAL=800 #改成自己的预定额度，建议稍小些，单位GB;
 TRAFF_DAY_TOTAL=5 #改成自己的预定额度，建议稍小些，单位GB;
 
-
-
 TRAFF_USED=$(vnstat --oneline b | awk -F';' '{print $11}')
 MONTH_GB=$(expr $TRAFF_USED / 1073741824)
 
